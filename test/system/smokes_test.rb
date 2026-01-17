087b3c3098e72c4866a6ad734023978e1e9feb53
duy <duy211099@gmail.com>
2026-01-17 13:50:24 +0700

    try fix test-system

---
+++ test/system/smokes_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class SmokesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit smokes_url
+  #
+  #   assert_selector "h1", text: "Smoke"
+  # end
+end
