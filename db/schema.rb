872c537eb6a73684ab99127f15d8733fa0e574b5
duy <duy211099@gmail.com>
2026-09-26 01:22:39 +0700

    chore: refresh schema dump and spec annotations

--- db/schema.rb
+++ db/schema.rb
@@ -12,28 +12,28 @@
 
 ActiveRecord::Schema[8.1].define(version: 2026_01_17_054635) do
   create_table "items", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.text "description"
-    t.datetime "discarded_at"
     t.string "name", null: false
-    t.string "phone_number"
-    t.datetime "updated_at", null: false
+    t.text "description"
     t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "phone_number"
+    t.datetime "discarded_at"
     t.index ["discarded_at"], name: "index_items_on_discarded_at"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "avatar_url"
-    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name"
-    t.string "provider"
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "provider"
     t.string "uid"
+    t.string "name"
+    t.string "avatar_url"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
@@ -41,13 +41,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_17_054635) do
   end
 
   create_table "versions", force: :cascade do |t|
+    t.string "whodunnit"
     t.datetime "created_at"
-    t.string "event", null: false
     t.bigint "item_id", null: false
     t.string "item_type", null: false
+    t.string "event", null: false
     t.text "object", limit: 1073741823
     t.text "object_changes"
-    t.string "whodunnit"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -2,6 +2,29 @@
 
 require "rails_helper"
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  avatar_url             :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  name                   :string
+#  provider               :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  uid                    :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_provider_and_uid      (provider,uid) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 RSpec.describe UserSerializer do
   fixtures :users
 
